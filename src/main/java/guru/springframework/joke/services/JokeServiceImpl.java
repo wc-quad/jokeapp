b0ea7a7413cdebdab9a0056ebd7493423818ddd2
Gabe Mabry <gsmabry@ncsu.edu>
2017-09-08 20:11:08 -0400

    add @Service JokeService with implementation

---
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,21 @@
+package guru.springframework.joke.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+	
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+	@Override
+	public String getJoke() {
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+
+}
